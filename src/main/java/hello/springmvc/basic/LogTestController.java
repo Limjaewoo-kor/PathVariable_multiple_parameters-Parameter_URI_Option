596b2d9b5564249609267e46e156cc938b8fae95
Limjaewoo <ljw3944@naver.com>
2023-01-02 00:57:32 +0900

    spring logger

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,22 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+        log.info("info log ={}", name);
+        log.trace("trace log={}",name);
+        log.debug("debug log={}",name);
+        log.warn("warn log={}",name);
+        log.error("error log={}",name);
+        return "ok";
+    }
+}

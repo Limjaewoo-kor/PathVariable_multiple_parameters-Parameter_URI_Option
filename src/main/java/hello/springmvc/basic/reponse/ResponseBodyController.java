373a7dbfb092e8cf353ab00c26c7dfd2ee69d352
Limjaewoo <ljw3944@naver.com>
2023-01-05 20:48:42 +0900

    json

--- src/main/java/hello/springmvc/basic/reponse/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/reponse/ResponseBodyController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -39,5 +40,15 @@ public class ResponseBodyController {
         helloData.setAge(20);
 
         return new ResponseEntity<>(helloData,HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/reponse-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return helloData;
     }
 }
